c3513f59f7ade36e08c3d0538ba746f69a2ecf10
Joao Grassi <joaop.grassi@gmail.com>
2017-09-23 16:38:13 +0200

    Adding header to pages and changing from css to use sass

--- src/app/_layout/header.component.ts
+++ src/app/_layout/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   template: `
-    <md-toolbar id="appToolbar" color="primary">
+    <md-toolbar id="appToolbar" class="mat-elevation-z4" color="primary">
       <span>Env-Magic</span>
       <span>No more mistakes in production!</span>
     </md-toolbar>
