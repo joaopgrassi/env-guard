f5eba1f8106e6b08f7726ef128cbae1550d2a606
Joao Grassi <joaop.grassi@gmail.com>
2017-12-05 20:08:30 +0100

    Refactoring content file and addind listener for extension install

--- src/content.js
+++ src/content.js
@@ -11,39 +11,34 @@
 
     applyRules = function () {
       for (var i = 0; i < allRules.length; i++) {
-          switch (allRules[i].operator) {
-            case 'Starts with':
-              if (location.href.startsWith(allRules[i].url) === true) {
-                rule = allRules[i];
-                break;
-              }
-              break;
-            case 'Ends with':
-              if (location.href.endsWith(allRules[i].url) === true) {
-                rule = allRules[i];
-                break;
-              }
-              break;
-            case 'Regex':
-              var regexp = new RegExp(allRules[i].url);
-              if (regexp.test(location.href) === true) {
-                rule = allRules[i];
-                break;
-              }
-              break;
-            case 'Contains':
-              if (location.href.indexOf(allRules[i].url) !== -1) {
-                rule = allRules[i];
-                break;
-              }
-              break;
-            case 'Exact':
-              if (location.href === allRules[i].url) {
-                rule = allRules[i];
-                break;
-              }
-              break;
-          }
+        switch (allRules[i].operator) {
+          case 'Starts with':
+            if (location.href.startsWith(allRules[i].url) === true) {
+              rule = allRules[i];
+            }
+            break;
+          case 'Ends with':
+            if (location.href.endsWith(allRules[i].url) === true) {
+              rule = allRules[i];
+            }
+            break;
+          case 'Regex':
+            var regexp = new RegExp(allRules[i].url);
+            if (regexp.test(location.href) === true) {
+              rule = allRules[i];
+            }
+            break;
+          case 'Contains':
+            if (location.href.indexOf(allRules[i].url) !== -1) {
+              rule = allRules[i];
+            }
+            break;
+          case 'Exact':
+            if (location.href === allRules[i].url) {
+              rule = allRules[i];
+            }
+            break;
+        }
       }
 
       // no rule, nothing we can do.
@@ -51,27 +46,20 @@
         return;
       }
 
-      function changeTabIcon (iconPath) {
-        var el, icon, link;
+      function changeTabIcon(iconPath) {
+        var el = document.querySelectorAll('head link[rel*="icon"]');
+        var icon = chrome.extension.getURL('/assets/' + iconPath);
 
-        el = document.querySelectorAll('head link[rel*="icon"]');
-
-        // Remove existing favicons
         Array.prototype.forEach.call(el, function (node) {
           node.parentNode.removeChild(node);
         });
 
-        icon = chrome.extension.getURL('/assets/' + iconPath);
-
-        // Create new favicon
-        link = document.createElement('link');
+        var link = document.createElement('link');
         link.type = 'image/x-icon';
         link.rel = 'shortcut icon';
         link.href = icon;
 
         document.getElementsByTagName('head')[0].appendChild(link);
-
-        return true;
       }
 
       if (rule.title !== null) {
@@ -84,7 +72,6 @@
         changeTabIcon(rule.icon.path);
       }
     };
-
     applyRules();
   });
 })();
--- src/eventPage.js
+++ src/eventPage.js
@@ -3,16 +3,22 @@ var extensionUrl = chrome.extension.getURL('index.html');
 openExtension = function () {
   chrome.tabs.query({ title: '*Env-Guard*' }, function (tabsArray) {
     if (tabsArray && tabsArray.length > 0) {
-      // if extension is already open  set it as active, preventing from having multiple instances of it.
+      // checks if extension is already open
       chrome.tabs.update(tabsArray[0].id, { active: true, highlighted: true }, function (tab) {
         chrome.windows.update(tab.windowId, { focused: true });
       });
     } else {
-      chrome.tabs.create({'url': extensionUrl, 'selected': true});
+      chrome.tabs.create({ 'url': extensionUrl, 'selected': true });
     }
   });
 };
 
-chrome.browserAction.onClicked.addListener(function() {
+chrome.browserAction.onClicked.addListener(function () {
   openExtension();
+});
+
+chrome.runtime.onInstalled.addListener(function (details) {
+  if (details.reason === 'install') {
+    openExtension('install');
+  }
 });
